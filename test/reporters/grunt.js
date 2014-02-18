d05f7b8cb8afede8d219ea012f3a637a5a054f15
Steve Elliott <steve@tegud.net>
2014-02-18 23:03:20 +0000

    Full Grunt reporter started
    TDD'ed, will replace original grunt-basic

--- test/reporters/grunt.js
+++ test/reporters/grunt.js
@@ -1,4 +1,28 @@
 (function () {
     'use strict';
 
+    var expect = require('expect.js');
+    var reporter = require('../../lib/reporters/grunt');
+    var EventEmitter = require('events').EventEmitter;
+    var grunt = require('grunt');
+
+    describe('given testRunBegin event is emitted', function() {
+        describe('and no tests have been configured', function() {
+            it('then it writes sub header indicating there are no tests', function() {
+                var actualMessage;
+                var expectedMessage = 'Executing Juve, no tests configured...';
+                var eventEmitter = new EventEmitter();
+
+                grunt.log.subhead = function(message) {
+                    actualMessage = message;
+                };
+
+                new reporter(grunt, eventEmitter);
+
+                eventEmitter.emit('testRunBegin', { tests: [] });
+
+                expect(actualMessage).to.be(expectedMessage);
+            });
+        });
+    });
 })();
