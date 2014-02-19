54ed9876bce673bd9591399074fb77caeea92290
Steve Elliott <steve@tegud.net>
2014-02-19 23:41:02 +0000

    Test Begin and error event basic info outputted.

--- test/reporters/grunt.js
+++ test/reporters/grunt.js
@@ -24,5 +24,59 @@
                 expect(actualMessage).to.be(expectedMessage);
             });
         });
+
+        describe('and one test has been configured', function() {
+            it('then it writes sub header indicating there is one test', function() {
+                var actualMessage;
+                var expectedMessage = 'Executing Juve, 1 test...';
+                var eventEmitter = new EventEmitter();
+
+                grunt.log.subhead = function(message) {
+                    actualMessage = message;
+                };
+
+                new reporter(grunt, eventEmitter);
+
+                eventEmitter.emit('testRunBegin', { tests: [{}] });
+
+                expect(actualMessage).to.be(expectedMessage);
+            });
+        });
+
+        describe('an error is raised', function() {
+            it('then it writes error message to the log', function() {
+                var actualMessage;
+                var expectedMessage = 'Could not find file';
+                var eventEmitter = new EventEmitter();
+
+                grunt.fail.warn = function() {};
+
+                grunt.log.error = function(message) {
+                    actualMessage = message;
+                };
+
+                new reporter(grunt, eventEmitter);
+
+                eventEmitter.emit('error', { message: expectedMessage });
+
+                expect(actualMessage).to.be(expectedMessage);
+            });
+
+            it('then it fails grunt with a general message', function() {
+                var actualMessage;
+                var expectedMessage = 'Juve failed due to error(s)';
+                var eventEmitter = new EventEmitter();
+
+                grunt.fail.warn = function(message) {
+                    actualMessage = message;
+                };
+
+                new reporter(grunt, eventEmitter);
+
+                eventEmitter.emit('error', { message: expectedMessage });
+
+                expect(actualMessage).to.be(expectedMessage);
+            });
+        });
     });
 })();
